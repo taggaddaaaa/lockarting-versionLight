5d28bfef61482541089c265a115673e7b70bbfa4
taggaddaaaa <sabinecaizergues@hotmail.com>
2020-01-14 19:12:13 +0100

    Add Hotjar service

--- index.php
+++ index.php
@@ -130,6 +130,18 @@
                    src="https://www.facebook.com/tr?id=741751919590920&ev=PageView&noscript=1"
         /></noscript>
     <!-- End Facebook Pixel Code -->
+
+    <!-- HOTJAR Tracking Code for www.lockarting.fr -->
+    <script>
+        (function(h,o,t,j,a,r){
+            h.hj=h.hj||function(){(h.hj.q=h.hj.q||[]).push(arguments)};
+            h._hjSettings={hjid:1642769,hjsv:6};
+            a=o.getElementsByTagName('head')[0];
+            r=o.createElement('script');r.async=1;
+            r.src=t+h._hjSettings.hjid+j+h._hjSettings.hjsv;
+            a.appendChild(r);
+        })(window,document,'https://static.hotjar.com/c/hotjar-','.js?sv=');
+    </script>
 </head>
 
 <body id="page-top">
