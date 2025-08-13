fec6d6ff6ffee5396bcb48f89e52bbe2ef8a1bb8
Sabine Caizergues <dev@sabinecaizergues.fr>
2025-08-13 14:24:33 +0200

    fix a bad comment in a js file

--- assets/js/lockarting.js
+++ assets/js/lockarting.js
@@ -78,24 +78,24 @@ function show_element(id) {
 }
 
 
-< !------------------------------------------->
-< !--TRY TO MAKE SOME RULES ON FORM INPUTS-- >
-< !------------------------------------------->
-    //Dans un input avec cette classe 'alpha-only', on accepte seulement les lettres et autres caracteres d'edition de texte
-    $(".alpha-only")
-        .on("keydown", function (event) {
-            // Ignore controls such as backspace
-            let arr = [8, 16, 17, 20, 35, 36, 37, 38, 39, 40, 45, 46];
+// -------------------------------------------
+// TRY TO MAKE SOME RULES ON FORM INPUTS
+// -------------------------------------------
+// Dans un input avec cette classe 'alpha-only', on accepte seulement les lettres et autres caracteres d'edition de texte
+$(".alpha-only")
+    .on("keydown", function (event) {
+        // Ignore controls such as backspace
+        let arr = [8, 16, 17, 20, 35, 36, 37, 38, 39, 40, 45, 46];
 
-            // Allow letters
-            for (let i = 65; i <= 90; i++) {
-                arr.push(i);
-            }
+        // Allow letters
+        for (let i = 65; i <= 90; i++) {
+            arr.push(i);
+        }
 
-            if (jQuery.inArray(event.which, arr) === -1) {
-                event.preventDefault();
-            }
-        })
-        .on("input", function () {
-            let regexp = /[^a-zA-Z]/g;
-        });
+        if (jQuery.inArray(event.which, arr) === -1) {
+            event.preventDefault();
+        }
+    })
+    .on("input", function () {
+        let regexp = /[^a-zA-Z]/g;
+    });
